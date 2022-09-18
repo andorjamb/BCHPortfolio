9d4f45366883340fed957c098c51918d206c8266
Anna Petelin <adpetelin@gmail.com>
2022-09-18 13:20:27 +0300

    changes to styling, started on JS file

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 
-
+console.log("testing connection");
 const backToTop = document.getElementById('back-to-top');
 backToTop.addEventListener('click', function () { window.scroll(0, 0) });
 
 
 
-window.addEventListener('scroll',)
-
+/* window.addEventListener('scroll',)
+ */
 
 function scrollDownEvents() {
     backToTop.style.visibility = visible;
 }
+
+const messageButton = document.
+    getElementById("messageButton");
+const logo4 = document.getElementById("logo4");
+messageButton.addEventListener("submit", function (event) {
+    event.preventDefault();
+    logo4.classList.add("rotate");
+})
