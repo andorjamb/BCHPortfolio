e51aca5ad256848ec9e11e6b35e2317889376a08
Anna Petelin <adpetelin@gmail.com>
2022-09-18 10:18:25 +0300

    attempting to solve merge conflicts

---
+++ main.js
@@ -0,0 +1,13 @@
+
+
+const backToTop = document.getElementById('back-to-top');
+backToTop.addEventListener('click', function () { window.scroll(0, 0) });
+
+
+
+window.addEventListener('scroll',)
+
+
+function scrollDownEvents() {
+    backToTop.style.visibility = visible;
+}
