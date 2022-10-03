3389938c7b1ebd8aa089bccea76ba99d68aa67dd
andor <andorjamb@protonmail.com>
2022-10-03 19:44:53 +0300

    fix title position

--- main.js
+++ main.js
@@ -1,22 +1,29 @@
 
+
+
 console.log("testing connection");
 const backToTop = document.getElementById('back-to-top');
-backToTop.addEventListener('click', function () { window.scroll(0, 0) });
 const form = document.querySelector('form');
+const sendBtn = document.form.querySelector("#sendBtn");
+const logo4 = document.querySelector('#logo4');
+const navOverlay = document.querySelector('.nav-overlay');
+const logoOverlay = document.querySelector('.logoOverlay');
 
-
-
-/* window.addEventListener('scroll',)
- */
 
 function scrollDownEvents() {
     backToTop.style.visibility = visible;
+    navOverlay.style.classList.add();
+    logoOverlay.style.classList.remove()
+
 }
 
-const submitMessage = document.form.
-    querySelector("#submitMessage");
-const logo4 = document.querySelector('#logo4');
+
+
 submitMessage.addEventListener("submit", function (event) {
     event.preventDefault();
    logo4.classList.add("rotate") ;
 })
+
+backToTop.addEventListener('click', function () { window.scroll(0, 0) });
+
+window.onscroll = function() {scrollDownEvents();}
