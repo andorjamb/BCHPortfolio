7f8089123cade1af6af9d6aad94f037a264c88ed
andor <andorjamb@protonmail.com>
2022-10-08 19:38:35 +0300

    fix header size and flex, fix scroll display of backtotop

--- main.js
+++ main.js
@@ -4,27 +4,33 @@
 console.log("testing connection");
 const backToTop = document.getElementById('back-to-top');
 const form = document.querySelector('form');
-const sendBtn = document.form.querySelector("#sendBtn");
+const nav = document.querySelector('nav');
 const logo4 = document.querySelector('#logo4');
 const navOverlay = document.querySelector('.nav-overlay');
 const logoOverlay = document.querySelector('.logoOverlay');
 
 
 function scrollDownEvents() {
-    backToTop.style.display = 'block';
-    navOverlay.style.classList.add();
-    logoOverlay.style.classList.remove();
-    navbar.style.backgroundColor = "var(--primary-apple-red)";
-
+    if (document.documentElement.scrollTop > 100){
+    console.log("activating scrolldown");
+     nav.classList.add("scrollDown");
+      backToTop.style.display = 'block';}
+    else {
+        nav.className = "";
+        backToTop.style.display = 'none';}
+   
+   /*  navOverlay.classList.add();
+    logoOverlay.classList.remove(); */
 }
 
 
-
+/* 
 submitMessage.addEventListener("submit", function (event) {
     event.preventDefault();
    logo4.classList.add("rotate") ;
-})
+}) */
 
 backToTop.addEventListener('click', function () { window.scroll(0, 0) });
 
-window.onscroll = function() {scrollDownEvents();}
+window.onscroll = function () {scrollDownEvents()};
+
