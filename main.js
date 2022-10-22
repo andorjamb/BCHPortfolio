8ee9b2107b5aa8e4c0780b4397cca4fb34afe5c7
Anna Petelin <adpetelin@gmail.com>
2022-10-22 19:35:23 +0300

    fiddling with css

--- main.js
+++ main.js
@@ -1,36 +1,47 @@
 
-
-
 console.log("testing connection");
 const backToTop = document.getElementById('back-to-top');
 const form = document.querySelector('form');
 const nav = document.querySelector('nav');
-const logo4 = document.querySelector('#logo4');
 const navOverlay = document.querySelector('.nav-overlay');
 const logoOverlay = document.querySelector('.logoOverlay');
+const menuIcon = document.querySelector('.menu-icon');
 
 
 function scrollDownEvents() {
-    if (document.documentElement.scrollTop > 100){
-    console.log("activating scrolldown");
-     nav.classList.add("scrollDown");
-      backToTop.style.display = 'block';}
+    if (document.documentElement.scrollTop > 100) {
+        console.log("activating scrolldown");
+        nav.classList.add("scrollDown");
+        backToTop.style.display = 'block';
+    }
     else {
         nav.className = "";
-        backToTop.style.display = 'none';}
-   
-   /*  navOverlay.classList.add();
-    logoOverlay.classList.remove(); */
+        backToTop.style.display = 'none';
+    }
+
+    /*  navOverlay.classList.add();
+     logoOverlay.classList.remove(); */
+}
+
+function mobileMenu() {
+    if (nav.classList.contains('responsive')) {
+        navOverlay.classList.remove('responsive');
+        document.body.style.overflow = "";
+    } else {
+        nav.classList.add('responsive');
+        document.body.style.overflow = "hidden";
+    }
 }
 
 
-/* 
-submitMessage.addEventListener("submit", function (event) {
+
+form.addEventListener("submit", function (event) {
     event.preventDefault();
-   logo4.classList.add("rotate") ;
-}) */
+})
 
 backToTop.addEventListener('click', function () { window.scroll(0, 0) });
 
-window.onscroll = function () {scrollDownEvents()};
+window.onscroll = function () { scrollDownEvents() };
+
+menuIcon.addEventListener('click', mobileMenu());
 
