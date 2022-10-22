98feb91efe0ec63140dfdd2ac8792be6a4042c2d
Anna Petelin <adpetelin@gmail.com>
2022-10-22 21:34:48 +0300

    add hamburger mobile menu

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ console.log("testing connection");
 const backToTop = document.getElementById('back-to-top');
 const form = document.querySelector('form');
 const nav = document.querySelector('nav');
+const navLinks = document.querySelectorAll('nav ul li a');
 const navOverlay = document.querySelector('.nav-overlay');
 const logoOverlay = document.querySelector('.logoOverlay');
 const menuIcon = document.querySelector('.menu-icon');
@@ -11,7 +12,7 @@ const menuIcon = document.querySelector('.menu-icon');
 function scrollDownEvents() {
     if (document.documentElement.scrollTop > 100) {
         console.log("activating scrolldown");
-        nav.classList.add("scrollDown");
+        nav.classList.add("scroll-down");
         backToTop.style.display = 'block';
     }
     else {
@@ -19,17 +20,18 @@ function scrollDownEvents() {
         backToTop.style.display = 'none';
     }
 
-    /*  navOverlay.classList.add();
-     logoOverlay.classList.remove(); */
 }
 
 function mobileMenu() {
-    if (nav.classList.contains('responsive')) {
-        navOverlay.classList.remove('responsive');
-        document.body.style.overflow = "";
-    } else {
+    /* for (const link of navLinks) {
+        link.addEventListener('click', mobileMenu())
+    } */
+    if (!nav.classList.contains('responsive')) {
         nav.classList.add('responsive');
-        document.body.style.overflow = "hidden";
+        //document.body.style.overflow = "hidden";
+    } else {
+        nav.classList.remove('responsive');
+        document.body.style.overflow = "";
     }
 }
 
@@ -37,11 +39,15 @@ function mobileMenu() {
 
 form.addEventListener("submit", function (event) {
     event.preventDefault();
+    console.log('form submitted');
 })
 
 backToTop.addEventListener('click', function () { window.scroll(0, 0) });
 
 window.onscroll = function () { scrollDownEvents() };
 
-menuIcon.addEventListener('click', mobileMenu());
+menuIcon.addEventListener('click', function () {
+    console.log('hamburger clicked');
+    mobileMenu();
+});
 
